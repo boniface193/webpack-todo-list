3ccc5f545fe7cbd6a5c7c1f68332d15f53f7c119
deboik193 <deboik193@gmail.com>
2023-03-29 20:20:08 +0100

    linter and dev env setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+import './style.css';
+
 const todos = [
-  {description: "eating two plates of food every lunch", completed: false, index: 0},
-  {description: "taking my bath every hour", completed: false, index: 1},
-  {description: "taking a nap every 8hrs a day", completed: false, index: 2},
-  {description: "No reasons", completed: false, index: 3},
-  {description: "no comments", completed: false, index: 4},
+  { description: "eating two plates of food every lunch", completed: false, index: 0 },
+  { description: "taking my bath every hour", completed: false, index: 1 },
+  { description: "taking a nap every 8hrs a day", completed: false, index: 2 },
+  { description: "No reasons", completed: true, index: 3 },
+  { description: "no comments", completed: false, index: 4 },
 ]
 
+const viewList = document.getElementById('viewList')
+
 const showList = () => {
-  console.log(todos)
+  viewList.innerHTML = ""
+
+  todos.forEach(listItem => {
+    viewList.innerHTML += `
+        <li class="d-flex py-4 align-center justify-between h-rule">
+          <input type="checkbox">
+          <div>${listItem.description}</div>
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </li>`
+  })
 }
 
 showList()
